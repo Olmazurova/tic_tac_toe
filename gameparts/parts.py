fd0892a4a25f63140aee6ada771008de103bc77e
Olga Mazurova <vsilena@mail.ru>
2024-11-22 19:48:24 +0300

    Добавлено описание класса Board

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,4 +1,8 @@
 class Board:
+    """Класс, который описывает игровое поле"""
+
+    field_size = 3
+
     def __init__(self):
         self.board = [[" " for _ in range(3)] for _ in range(3)]
 
@@ -13,3 +17,9 @@ class Board:
         for row in self.board:
             print(*row, sep=' | ')
             print('-' * 10)
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        ) 
